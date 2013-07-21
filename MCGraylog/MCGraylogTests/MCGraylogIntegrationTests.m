7d697e78f971a8d3e872a3afd6715502e486ba13
Mark Rada <marada@uwaterloo.ca>
2013-07-21 17:15:50 -0400

    Clean up tests#logstashResponse

--- MCGraylog/MCGraylogTests/MCGraylogIntegrationTests.m
+++ MCGraylog/MCGraylogTests/MCGraylogIntegrationTests.m
@@ -50,24 +50,34 @@ static NSPipe* outputPipe;
     graylog_init("localhost", "12201");
 }
 
+
 - (void)tearDown {
     [super tearDown];
     graylog_deinit();
 }
 
-- (NSDictionary *)logstashResponse {
-    NSMutableData * data = [NSMutableData data];
+
+- (NSDictionary*) logstashResponse {
+
+    NSMutableData* data = [NSMutableData data];
     do {
         [data appendData:[outputPipe.fileHandleForReading availableData]];
-        NSLog(@"%lu", data.length);
-    } while (data.length < 10);
+    } while (data.length < 10); // TODO: why 10? magic number fuuuuuu
     
-    NSError * jsonParseError = nil;
-    
-    NSDictionary * logstashResponse = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonParseError];
-    STAssertNotNil(logstashResponse, @"Failed to parse logstash response:%@. Error: %@", [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
-    return logstashResponse;
+    NSError*   parse_error = nil;
+    NSDictionary* response = [NSJSONSerialization JSONObjectWithData:data
+                                                             options:0
+                                                               error:&parse_error];
+    STAssertNotNil(response,
+                   @"Failed to parse logstash response: %@. Error: %@",
+                   [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding],
+                   parse_error);
+
+    return response;
 }
+
+
+#pragma mark Tests
 
 - (void)testBasicLogging {
     graylog_log(GraylogLogLevelInformational, @"test", @"message", @{});
@@ -75,11 +85,14 @@ static NSPipe* outputPipe;
     STAssertEqualObjects(response[@"@fields"][@"short_message"], @"message", @"Short message should be test");
     STAssertEqualObjects(response[@"@fields"][@"facility"], @"test", @"Short message should be test");
 }
+
+
 - (void)testDebugLevel {
     graylog_log(GraylogLogLevelDebug, @"test", @"message", @{});
     NSDictionary * response = [self logstashResponse];
     STAssertEqualObjects(response[@"@fields"][@"level"], @(GraylogLogLevelDebug), @"Wrong log level in response");
 }
+
 
 - (void)testAlertLevel {
     graylog_log(GraylogLogLevelAlert, @"test", @"message", @{});
@@ -87,11 +100,13 @@ static NSPipe* outputPipe;
     STAssertEqualObjects(response[@"@fields"][@"level"], @(GraylogLogLevelAlert), @"Wrong log level in response");
 }
 
+
 - (void)testCriticalLevel {
     graylog_log(GraylogLogLevelCritical, @"test", @"message", @{});
     NSDictionary * response = [self logstashResponse];
     STAssertEqualObjects(response[@"@fields"][@"level"], @(GraylogLogLevelCritical), @"Wrong log level in response");
 }
+
 
 - (void)testEmergencyLevel {
     graylog_log(GraylogLogLevelEmergency, @"test", @"message", @{});
@@ -99,11 +114,13 @@ static NSPipe* outputPipe;
     STAssertEqualObjects(response[@"@fields"][@"level"], @(GraylogLogLevelEmergency), @"Wrong log level in response");
 }
 
+
 - (void)testErrorLevel {
     graylog_log(GraylogLogLevelError, @"test", @"message", @{});
     NSDictionary * response = [self logstashResponse];
     STAssertEqualObjects(response[@"@fields"][@"level"], @(GraylogLogLevelError), @"Wrong log level in response");
 }
+
 
 - (void)testInformationalLevel {
     graylog_log(GraylogLogLevelInformational, @"test", @"message", @{});
@@ -111,11 +128,13 @@ static NSPipe* outputPipe;
     STAssertEqualObjects(response[@"@fields"][@"level"], @(GraylogLogLevelInformational), @"Wrong log level in response");
 }
 
+
 - (void)testNoticeLevel {
     graylog_log(GraylogLogLevelNotice, @"test", @"message", @{});
     NSDictionary * response = [self logstashResponse];
     STAssertEqualObjects(response[@"@fields"][@"level"], @(GraylogLogLevelNotice), @"Wrong log level in response");
 }
+
 
 - (void)testWarningLevel {
     graylog_log(GraylogLogLevelWarning, @"test", @"message", @{});
@@ -123,16 +142,19 @@ static NSPipe* outputPipe;
     STAssertEqualObjects(response[@"@fields"][@"level"], @(GraylogLogLevelWarning), @"Wrong log level in response");
 }
 
+
 - (void)testCustomTextField {
     graylog_log(GraylogLogLevelWarning, @"test", @"message", @{@"custom_field":@"hello"});
     NSDictionary * response = [self logstashResponse];
     STAssertEqualObjects(response[@"@fields"][@"_custom_field"], @"hello",@"");
 }
 
+
 - (void)testCustomNumberField {
     graylog_log(GraylogLogLevelWarning, @"test", @"message", @{@"custom_field":@5});
     NSDictionary * response = [self logstashResponse];
     STAssertEqualObjects(response[@"@fields"][@"_custom_field"], @5,@"");
 }
+
 
 @end
